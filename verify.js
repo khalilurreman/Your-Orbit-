83d05b44f6670da366b1a9e54b9866f3afb0f13a
khalilurreman <kjarral440@gmail.com>
2026-01-19 19:58:01 +0400

    Create verify.js

---
+++ verify.js
@@ -0,0 +1,4 @@
+function addBlueTick(userName) {
+    const tick = '<svg class="w-4 h-4 text-blue-500 inline" fill="currentColor" viewBox="0 0 20 20"><path d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"></path></svg>';
+    return userName + " " + tick;
+}
